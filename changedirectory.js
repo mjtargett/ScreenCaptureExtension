37e730c262a8dd95339e9bb193f23833ac5a3756
Michael Targett <miketargett10@gmail.com>
2023-08-08 21:59:25 -0400

    Fixed changedirectory.js using the chrome.tabs api

--- changedirectory.js
+++ changedirectory.js
@@ -1,4 +1,4 @@
 /* upon button press, sends the user to chrome://settings/downloads which allows them to change the directory of saved files */
-document.getElementById("directory-button").addEventListener(click, function() {
-  window.location.href = "chrome://settings/downloads";
-})
+document.getElementById("directory-button").addEventListener("click", function() {
+    chrome.tabs.update({url: "chrome://settings/downloads"});
+  });
