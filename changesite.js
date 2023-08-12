e9d495fd91e3a22923f22826ade702a92a8e8811
Michael Targett <miketargett10@gmail.com>
2023-08-12 16:26:34 -0400

    Minor fixes

--- changesite.js
+++ changesite.js
@@ -1,9 +1,9 @@
 /* sends the user to chrome://settings/downloads upon button press which allows them to change the directory of saved files */
 document.getElementById("directory-button").addEventListener("click", function() {
-    chrome.tabs.update({url: "chrome://settings/downloads"});
+    chrome.tabs.create({url: "chrome://settings/downloads"});
 });
 
 /* sends the user to the author's github upon button press */
-document.getElementById("github-button").addEventListener("click", function() {
-  chrome.tabs.update({url: "https://github.com/mjtargett"});
+document.getElementById("github-link").addEventListener("click", function() {
+  chrome.tabs.create({url: "https://github.com/mjtargett"});
 });
